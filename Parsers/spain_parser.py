56d52f65ebf0656f2d3bbb5d1cfb261ecc5bf308
Kajaqq <kajos200@gmail.com>
2026-02-24 15:36:07 +0100

    Split Spain into Downloader/Parser and simplify code structure.

--- Parsers/spain_parser.py
+++ Parsers/spain_parser.py
@@ -1,37 +1,14 @@
 import json
-from base64 import b64decode
 from collections import defaultdict
 from pathlib import Path
 
-from tools.utils import download_post, save_json, xor_decode
-from config import CONSTANTS
-
-DATA_URL = CONSTANTS.SPAIN.CAMERA_API
-XOR_KEY = CONSTANTS.SPAIN.XOR_KEY
+from tools.utils import save_json
+from tools.spain_downloader import get_spain_data
 
 
-async def get_camera_data():
-    download_link = DATA_URL
-    xored_data = await download_post(download_link)
-    decoded_data = decode_data(xored_data)
-    return decoded_data
-
-
-def decode_data(camaras_data):
+async def parse_camera_data(raw_data):
     try:
-        decoded_bytes = b64decode(camaras_data, validate=True)
-    except Exception as exc:
-        raise ValueError(f"Base64 decode failed: {exc}") from exc
-
-    json_text = xor_decode(decoded_bytes, XOR_KEY)
-
-    print("Successfully downloaded camera data.")
-    return json_text
-
-
-async def parse_camera_data(json_data):
-    try:
-        raw_data = json.loads(json_data)
+        raw_data = json.loads(raw_data)
     except json.JSONDecodeError:
         print("Error: Failed to decode the input JSON file.")
         return None
@@ -71,7 +48,7 @@ async def parse_camera_data(json_data):
 
 
 async def get_parsed_data(output_file=None, output_folder=None):
-    camera_data = await get_camera_data()
+    camera_data = await get_spain_data()
     spain_data = await parse_camera_data(camera_data)
     if output_file:
         save_json(spain_data, output_file)
@@ -84,5 +61,5 @@ if __name__ == "__main__":
     import asyncio
 
     OUTPUT_DIR = Path("../data/cameras_es_gov.json")
-    data = asyncio.run(get_camera_data())
+    data = asyncio.run(get_spain_data())
     asyncio.run(parse_camera_data(data))
---
+++ tools/spain_downloader.py
@@ -0,0 +1,25 @@
+from base64 import b64decode
+
+from config import CONSTANTS
+from tools.utils import download_post, xor_decode
+
+DATA_URL = CONSTANTS.SPAIN.CAMERA_API
+XOR_KEY = CONSTANTS.SPAIN.XOR_KEY
+
+
+def decode_data(camaras_data):
+    try:
+        decoded_bytes = b64decode(camaras_data, validate=True)
+    except Exception as exc:
+        raise ValueError(f"Base64 decode failed: {exc}") from exc
+
+    json_text = xor_decode(decoded_bytes, XOR_KEY)
+
+    print("Successfully downloaded camera data.")
+    return json_text
+
+async def get_spain_data():
+    download_link = DATA_URL
+    xored_data = await download_post(download_link)
+    decoded_data = decode_data(xored_data)
+    return decoded_data
